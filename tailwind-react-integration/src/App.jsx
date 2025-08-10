1b30acf2d79a75e1dafc75cbbde55baa85acfb36
Amos5041 <125496513+Amos5041@users.noreply.github.com>
2025-08-10 23:36:47 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,7 @@
 import UserProfile from "./components/UserProfile";
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-white">
       <UserProfile />
     </div>
   );
