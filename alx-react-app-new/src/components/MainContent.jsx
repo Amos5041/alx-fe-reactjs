9dfb13d818c9b3f98a07fd29e5777d136285178f
Amos5041 <125496513+Amos5041@users.noreply.github.com>
2025-08-10 18:44:46 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f8ff', padding: '15px', fontSize: '18px', lineHeight: '1.6', margin: '10px 0' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
