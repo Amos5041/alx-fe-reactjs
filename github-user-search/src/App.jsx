5fb9e609244f82a9bd194e9e496e8b0ad3bedcb2
Amos5041 <125496513+Amos5041@users.noreply.github.com>
2025-08-10 22:24:31 +0300

    Create App.jsx

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>GitHub User Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+export default App;
