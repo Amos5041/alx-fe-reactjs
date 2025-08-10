8d3e9cc305d8ba7e9ae4162ef947ca90d026ea3c
Amos5041 <125496513+Amos5041@users.noreply.github.com>
2025-08-10 22:51:54 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
-const githubApi = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: apiKey ? { Authorization: `token ${apiKey}` } : {}
-});
 export async function searchUsers({ username, location, minRepos, page = 1 }) {
   let query = '';
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos} `;
   try {
-    const response = await githubApi.get(`/search/users`, {
-      params: { q: query.trim(), page, per_page: 5 }
-    });
+    const response = await axios.get(
+      `https://api.github.com/search/users`,
+      {
+        params: {
+          q: query.trim(),
+          page,
+          per_page: 5
+        },
+        headers: apiKey ? { Authorization: `token ${apiKey}` } : {}
+      }
+    );
     return response.data;
   } catch (error) {
     throw error;
